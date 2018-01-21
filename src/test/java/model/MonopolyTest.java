170fa49b814b63d2ab30a9bec4f33ad3e22d3f46
deeptibn <deeptibn@inblr-deeptibn.local>
2018-01-21 12:57:02 +0530

    Add src directory with model package <Deepti>

---
+++ src/test/java/model/MonopolyTest.java
@@ -0,0 +1,15 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MonopolyTest {
+    @Test
+
+    public void testShouldTakeInputsAndDelegateInitialization() {
+        Monopoly monopoly = new Monopoly();
+
+        assertEquals(1, 1);
+    }
+}
