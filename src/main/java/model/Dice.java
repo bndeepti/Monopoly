d4aab393335a0c40fbf70bc7c1afad4b5bc1f1ec
deeptibn <deeptibn@inblr-deeptibn.local>
2018-01-21 19:25:28 +0530

    Initialize dice output

--- src/main/java/model/Dice.java
+++ src/main/java/model/Dice.java
@@ -1,7 +1,17 @@
 package model;
 
-public class Dice {
-    public void init(String diceOutput) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Dice {
+    private List<String> rollValues = new ArrayList<>();
+
+    public void init(String diceOutput) {
+        rollValues = Arrays.asList(diceOutput.split(","));
+    }
+
+    public int getRollValue(int index) {
+        return Integer.valueOf(rollValues.get(index));
     }
 }
---
+++ src/test/java/model/DiceTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiceTest {
+    Dice dice;
+    @Before
+    public void setUp() throws Exception {
+        dice = new Dice();
+    }
+
+    @Test
+    public void testShouldInitRollValues() {
+        String diceOutput = "4,5,6";
+
+        dice.init(diceOutput);
+
+        assertEquals(4, dice.getRollValue(0));
+        assertEquals(5, dice.getRollValue(1));
+        assertEquals(6, dice.getRollValue(2));
+    }
+
+}
