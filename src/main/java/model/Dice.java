ad8bd2a37bdc304d4eddff824e8de423e9f34661
deeptibn <deeptibn@inblr-deeptibn.local>
2018-01-22 20:13:17 +0530

    Add Dice tests for roll method

--- src/main/java/model/Dice.java
+++ src/main/java/model/Dice.java
@@ -1,17 +1,22 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Dice {
-    private List<String> rollValues = new ArrayList<>();
+    private List<String> rollValues;
+    private int rollNumber;
 
     public void init(String diceOutput) {
         rollValues = Arrays.asList(diceOutput.split(","));
+        rollNumber = 0;
     }
 
     public int getRollValue(int index) {
         return Integer.valueOf(rollValues.get(index));
+    }
+
+    public int roll() {
+        return getRollValue(rollNumber++ % rollValues.size());
     }
 }
--- src/test/java/model/DiceTest.java
+++ src/test/java/model/DiceTest.java
@@ -23,4 +23,15 @@ public class DiceTest {
         assertEquals(6, dice.getRollValue(2));
     }
 
+    @Test
+    public void testShouldGiveNextRollValue() {
+        String diceOutput = "4,5,6";
+
+        dice.init(diceOutput);
+
+        assertEquals(4, dice.roll());
+        assertEquals(5, dice.roll());
+        assertEquals(6, dice.roll());
+    }
+
 }
