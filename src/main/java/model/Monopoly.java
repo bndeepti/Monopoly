d663026d2a668d6cf137c817bcdd23222537ac39
deeptibn <deeptibn@inblr-deeptibn.local>
2018-01-22 20:26:43 +0530

    Init game players

--- src/main/java/model/Monopoly.java
+++ src/main/java/model/Monopoly.java
@@ -1,16 +1,30 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Monopoly {
     private Board board;
     private Dice dice;
+    private List<Player> players = new ArrayList<>();;
 
     public Monopoly(Board board, Dice dice) {
         this.board = board;
         this.dice = dice;
     }
 
-    public void init(String cellPositionsAndTypes, String diceOutput) {
+    public void init(String numberOfPlayers, String cellPositionsAndTypes, String diceOutput) {
         board.init("");
         dice.init("");
+        initPlayers(Integer.valueOf(numberOfPlayers));
+    }
+
+    private void initPlayers(int numberOfPlayers) {
+        for (int i = 1; i <= numberOfPlayers; i++)
+            players.add(new Player("Player-" + i));
+    }
+
+    public Player getPlayer(int index) {
+        return players.get(index);
     }
 }
--- src/test/java/model/MonopolyTest.java
+++ src/test/java/model/MonopolyTest.java
@@ -1,5 +1,6 @@
 package model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,16 +9,36 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class MonopolyTest {
+    Monopoly monopoly;
+    Board board;
+    Dice dice;
+
+    @Before
+    public void setUp() {
+        board = mock(Board.class);
+        dice = mock(Dice.class);
+        monopoly = new Monopoly(board, dice);
+    }
+
     @Test
     public void testShouldTakeInputsAndDelegateInitialization() {
-        Board board = mock(Board.class);
-        Dice dice = mock(Dice.class);
-        Monopoly monopoly = new Monopoly(board, dice);
-
         String cellPositionsAndTypes = "";
         String diceOutput = "";
-        monopoly.init(cellPositionsAndTypes, diceOutput);
+        String numberOfPlayers = "";
+        monopoly.init(numberOfPlayers, cellPositionsAndTypes, diceOutput);
         verify(board, times(1)).init(cellPositionsAndTypes);
         verify(dice, times(1)).init(diceOutput);
+    }
+
+    @Test
+    public void testShouldInitPlayers() {
+        String numberOfPlayers = "2";
+        String diceOutput = "";
+        String cellPositionAndTypes = "";
+
+        monopoly.init(numberOfPlayers, cellPositionAndTypes, diceOutput);
+
+        assertEquals("Player-1", monopoly.getPlayer(0).getName());
+        assertEquals("Player-2", monopoly.getPlayer(1).getName());
     }
 }
